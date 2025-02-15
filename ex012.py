400b6f921f50aec4d35cce18ec152de25b971807
Seu Nome <caiopolis13@gmail.com>
2025-02-15 17:05:07 -0300

    alterando frases

--- ex012.py
+++ ex012.py
@@ -1,4 +1,4 @@
 print('Olá, este programa irá lhe ajudar a calcular o aumento do seu salário')
-x = float(input('primeiramente, qual o seu salario? '))
-x2 = float(input('E quanto é a porcentagem de aumento (Obs:Somente o numero,sem o simbolo da porcentagem)? '))
+x = float(input('primeiramente, qual o seu salario, senhor(a)? '))
+x2 = float(input('E quanto é a porcentagem de aumento? (Obs:Somente o numero,sem o simbolo da porcentagem) '))
 print('Seu salário antigo era {}, com o aumento de {}%, seu novo salário é {}'.format(x,x2,x+x*(x2/100)))
--- ex021.py
+++ ex021.py
@@ -1,6 +1,6 @@
 nome = str(input('Digite o seu nome:\n')).strip()
 print('Então seu nome em caps lock é',nome.upper())
-print('Então seu nome em minusculo é:',nome.lower())
+print('Então seu nome em minusculo é...:',nome.lower())
 print('Seu nome tem {} letras'.format(len(nome)-nome.count(' ')))
 nome2 = nome.split()
 print('Seu primeiro nome é {}, e tem {} letras'.format(nome2[0],len(nome2[0])))
